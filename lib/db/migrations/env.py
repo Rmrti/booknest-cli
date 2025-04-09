eaa9a86b2cfeee310eb4f9d8139212d6c67d75f4
Rmrti <mutuma34@gmail.com>
2025-04-09 16:08:10 +0300

    done

--- lib/db/migrations/env.py
+++ lib/db/migrations/env.py
@@ -34,6 +34,7 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
+    with connectable.connect() as connection:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
